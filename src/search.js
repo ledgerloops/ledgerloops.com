0e358f5fe408644d38e49f4ea3afd25d81c6c347
Michiel de Jong <michiel@unhosted.org>
2016-10-21 16:28:22 +0700

    Code that should give each Search object a correct list of in- and out-neighbors, time to write some tests now probably

--- src/search.js
+++ src/search.js
@@ -7,10 +7,28 @@ function Search(messagesCallback) {
 }
 
 Search.prototype.onNeighborChange = function(neighborChange) {
-  this._inNeighbors[neighborNick] = neighborType['in'];
-  if (typeof this._outNeighbors[neighborNick] === 'undefined') {
-    this._outNeighbors[neighborNick] = {};
-  }
-  this._outNneighbors[neighborNick].active = neighborType;
+  switch (neighborChange.change) {
+   case neighborChangeEvents.CREDITOR_CREATED: 'nc-creditor-created':
+     this._inNeighbor[JSON.stringify([neighborChange.peerNick, neighborChange.currency])] = {};
+     break;
+   case neighborChangeEvents.CREDITOR_REMOVED: 'nc-creditor-removed':
+     delete this._inNeighbor[JSON.stringify([neighborChange.peerNick, neighborChange.currency])];
+     break;
+   case neighborChangeEvents.DEBTOR_CREATED: 'nc-debtor-created':
+     this._outNeighbor[JSON.stringify([neighborChange.peerNick, neighborChange.currency])] = {};
+     break;
+   case neighborChangeEvents.DEBTOR_REMOVED: 'nc-debtor-removed':
+     delete this._outNeighbor[JSON.stringify([neighborChange.peerNick, neighborChange.currency])];
+     break;
+   case neighborChangeEvents.DEBTOR_TO_CREDITOR: 'nc-debtor-to-creditor':
+     delete this._inNeighbor[JSON.stringify([neighborChange.peerNick, neighborChange.currency])];
+     this._inNeighbor[JSON.stringify([neighborChange.peerNick, neighborChange.currency])] = {};
+     break;
+   case neighborChangeEvents.CREDITOR_TO_DEBTOR: 'nc-creditor-to-debtor':
+     delete this._inNeighbor[JSON.stringify([neighborChange.peerNick, neighborChange.currency])];
+     this._outNeighbor[JSON.stringify([neighborChange.peerNick, neighborChange.currency])] = {};
+     // break;
+   }
 };
+
 module.exports = Search;
