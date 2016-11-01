029f68792d3313acf6c377cf382d8188bd563f1d
Michiel de Jong <michiel@unhosted.org>
2016-11-01 19:13:32 +0700

    Missing methods on Signatures wrapper around Challenge

--- src/signatures.js
+++ src/signatures.js
@@ -16,5 +16,19 @@ Signatures.prototype.haveKeypair = function(publicKeyBase64) {
   return (typeof this._challenges[publicKeyBase64] !== 'undefined');
 };
 
+Signatures.prototype.sign = function(cleartextStr, publicKeyBase64) {
+  return this._challenges[publickKeyBase64].solve();
+};
+
+Signatures.prototype.verify = function(cleartext, publicKeyBase64, signatureBase64) {
+  var tmpChallenge = new Challenge();
+  return tmpChallenge.fromData({
+    pubkey: publickeyBase64,
+    cleartext: cleartext,
+  }).then(() => {
+    return tmpChallenge.verifySolution(signatureBase64);
+  });
+};
+
 module.exports = Signatures;
 
