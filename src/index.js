b04e07ee164e611c8223c6cff04173cf3bee06a7
Michiel de Jong <michiel@unhosted.org>
2016-11-02 17:29:59 +0700

    Simplify demo to only 3 participants

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ var agents = {
   alice: new Agent('alice'),
   bob: new Agent('bob'),
   charlie: new Agent('charlie'),
-  daphne: new Agent('daphne'),
-  edward: new Agent('edward'),
-  fred: new Agent('fred'),
-  geraldine: new Agent('geraldine'),
 };
 
 // in this demo, all agents live within the same nodejs process, and send
@@ -27,15 +23,8 @@ messaging.autoFlush();
 agents.alice.sendIOU('bob', 0.1, 'USD');
 // alice will notify bob, and both will update their peer ledger.
 agents.bob.sendIOU('charlie', 0.1, 'USD');
-agents.charlie.sendIOU('daphne', 0.1, 'USD');
-agents.daphne.sendIOU('edward', 0.1, 'USD');
-agents.edward.sendIOU('fred', 0.1, 'USD');
-agents.fred.sendIOU('geraldine', 0.1, 'USD');
-setTimeout(() => {
-  agents.geraldine.sendIOU('alice', 0.1, 'USD');
-}, 5000);
-// at this point, geraldine will notify alice, and alice can use her credit with geraldine to settle her debt with bob.
-// note that alice does not know about the existence of charlie, daphne, edward, and fred.
+agents.charlie.sendIOU('alice', 0.1, 'USD');
+// at this point, charlie will notify alice, and alice can use her credit with charlie to settle her debt with bob.
 // each agent only knows, only interacts with, and only trusts their own direct debtors and creditors.
 // Yet the network can still find a second-order promise loop where all peer-to-peer debts are settled.
 // See console output for a log of the messages each agent sends in the process.
